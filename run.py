99191efc92c986999e495a450b6a9591f59b3865
wdsandeep <wd.sandeep@gmail.com>
2021-09-16 17:28:48 +0530

    Files commit first time

---
+++ run.py
@@ -0,0 +1,26 @@
+from booking.booking import Booking
+
+try:
+    with Booking() as bot:
+        bot.land_first_page()
+        bot.change_currency(currency='USD')
+        bot.select_place_to_go('New York')
+        bot.select_dates(check_in_date='2021-09-22', check_out_date='2021-09-30')
+        bot.select_adults(10)
+        bot.click_search()
+        bot.apply_filtration()
+        bot.refresh()
+        bot.report_results()
+
+except Exception as e:
+    if 'in PATH' in str(e):
+        print(
+            'You are trying to run the bot from command line \n'
+            'Please add to PATH your Selenium Drivers \n'
+            'Windows: \n'
+            '    set PATH=%PATH%;C:path-to-your-folder \n \n'
+            'Linux: \n'
+            '    PATH=$PATH:/path/toyour/folder/ \n'
+        )
+    else:
+        raise
